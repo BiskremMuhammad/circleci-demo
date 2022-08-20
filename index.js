3766527cf29a0efb3d45f8820bde88dc31897a28
Muhammad Omran <muhammad@surrealevents.com>
2022-08-20 21:39:46 +0200

    update package json to allow using EC6 module imports

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
   res.send({ results: "ok", message: "Welcome to Biskrem world" });
 });
 
-app.listen(5000, () => {
-  console.log("biskrem is listening on port 5000");
+app.listen(5555, () => {
+  console.log("biskrem is listening on port 5555");
 });
 
 export default app;
